21254a873787e6f7423d4746d59c61ac8bae2a1e
✭ ANGEL DOLLFACE ✭ <112771657+angeldollface@users.noreply.github.com>
2024-05-18 13:48:18 +0200

    Update mina.ts

--- src/mina.ts
+++ src/mina.ts
@@ -1,14 +1,1 @@
-function main(){
-	let inputField: Component = new Component(
-		{
-		}
-	);
-	let echo: Component = new Component(
-		{
-		}
-	);
-	let app: App = new App("example");
-	app.addComponent(inputField);
-	app.addComponent(echoField);
-	app.run();
-}
+// Coming.
