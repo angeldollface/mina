4410e89f1b011c086ea4dfb4fc1456f2a08ff4fe
✭ ANGEL DOLLFACE ✭ <112771657+angeldollface@users.noreply.github.com>
2024-05-18 18:24:42 +0200

    Update example.ts

--- example/example.ts
+++ example/example.ts
@@ -15,24 +15,18 @@ function decrementSubject(
 function main(){
 	let count: number;
 	let echoComponent: mina.Component = new mina.Component(
-		{
 			stateful: count,
 			elements: '<p>{{ count }}</p>'
-		}
 	);
 	let incrementController: mina.Controller = new mina.Controller(
-		{
 			subject: count,
 			agent: incrementSubject,
 			element: '<button onclick="{{ agent }}">+</button>'
-		}
 	);
 	let decrementController: mina.Controller = new mina.Controller(
-		{
 			subject: count,
 			agent: decrementSubject,
 			element: '<button onclick="{{ agent }}">-</button>'
-		}
 	);	
 	let counterApp: mina.App = new App("counter");
 	let topHtml: string = '<body><h1>MINA COUNTER APP</h1>';
