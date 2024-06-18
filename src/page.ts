1bc0f09e95b1078876a11d1be6584c59d04c49c7
Paul Eboselume <eboselumepaul@gmail.com>
2024-06-18 10:46:23 +0100

    Fixed the main bug and made little changes

--- src/page.ts
+++ src/page.ts
@@ -1,26 +1,25 @@
-import Tag from './tag.ts';
+import Tag from "./tag.ts";
 
 export class Page {
-    name: string;
-    tags: Array<Tag>;
+  name: string;
+  tags: Array<Tag>;
 
-    constructor(
-        name: string,
-    ){
-        this.name = name;
-        this.tags = [];
-    }
-    addTag(tag: Tag): void {
-        this.tags.push(tag);
-    }
+  constructor(name: string) {
+    this.name = name;
+    this.tags = [];
+  }
+  addTag(tag: Tag): void {
+    this.tags.push(tag);
+  }
 
-    toString(): string {
-        let html: string = '';
-        for (let i = 0; i < this.tags.length; i++){
-            html += this.tags[i].toString();
-        }
-        return html
+  toString(): string {
+    let html: string = "";
+    for (let i = 0; i < this.tags.length; i++) {
+      html += this.tags[i].toString();
     }
+    return html;
+  }
 }
 
 export default Page;
+
